2b5c4c6080dab3e2a38d51db2fe4be45fd3b5244
Daniel Vazquez <dnlvazquez98@gmail.com>
2021-10-02 16:20:16 -0500

    Add files via upload

---
+++ Template.py
@@ -0,0 +1,38 @@
+"""
+		Program: Arrays/List
+		 Author: Daniel Vazquez
+		   Date: 02/10/2018
+
+	 Assignment: Create array/list with 20 possible elements. Fill with 10
+				 random integers 1 <= n <= 100. Write a function to insert value
+				 in middle of list.
+
+	   Due Date:
+
+	  Objective: Write a function to insert value in middle of list.
+
+	Execute via: >>python vazquez_hw021218_v1.00
+
+		  To Do:
+   
+
+**				Template based on Dr. Nichols program example.				  **
+"""
+#-----------------------------------------------------------------------------80
+import time # Used to get current time and the timer for the program
+
+
+def main():
+	print("vazquez_hw021218_v1.00.py\n")
+	
+	
+	
+    disDate() # Prints date
+
+# Source:
+#    https://www.pythoncentral.io/how-to-display-the-date-and-time-using-python/
+def disDate():
+    print(time.strftime('%m/%d/%Y %H:%M'))
+
+#Call Main--------------------------------------------------------------------80
+main()
